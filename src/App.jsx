e3e1ae5a75ddc9d5689ffc71d9becb582e7b230f
Matheus <matheusramosbi.13@gmail.com>
2024-07-10 12:06:31 -0300

    UserContext and LoginForm improovements

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
