092013a5bf56b5ed33c70622cdbfe26fd60547cf
Matheus <matheusramosbi.13@gmail.com>
2024-07-18 10:17:57 -0300

    user Profile

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { UserStorage } from './UserContext';
 import User from './components/User/User';
 import ProtectedRoute from './components/Helper/ProtectedRoute';
 import Photo from './components/Photo/Photo';
+import UserProfile from './components/User/UserProfile';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
