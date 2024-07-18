61456744e39d122974966e3c5524207bbedca07e
Matheus <matheusramosbi.13@gmail.com>
2024-07-18 10:20:33 -0300

    error 404 page not found

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import User from './components/User/User';
 import ProtectedRoute from './components/Helper/ProtectedRoute';
 import Photo from './components/Photo/Photo';
 import UserProfile from './components/User/UserProfile';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
             />
             <Route path="foto/:id" element={<Photo />} />
             <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="container mainContainer">
+      <h1 className="tile">Error: 404</h1>
+      <p>Página não encontrada.</p>
+    </div>
+  );
+};
+
+export default NotFound;
